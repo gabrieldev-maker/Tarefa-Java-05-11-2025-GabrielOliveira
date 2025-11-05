6e99704ebe1bb8c07c2a6406b1532718bac1b1dd
gabrieldev-maker <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>
2025-11-05 18:16:05 -0300

    start commit

---
+++ Q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double salario, nsalario, aumento, percentual;
+        System.out.println("Digite o salário do funcionário: ");
+        salario = scanner.nextDouble();
+        
+        
+    }
+}
